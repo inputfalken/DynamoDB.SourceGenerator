273d6a0cafbb3258ed1776d3a7d3f8abeb1f53cc
Robert Andersson <robert.andersson@mathem.se>
2023-04-13 23:03:54 +0200

    Add more unit test coverage and improve readability of conversion

--- DynamoDBGenerator.SourceGenerator.Tests/AttributeValueConversion/Types/NumberTests.cs
+++ DynamoDBGenerator.SourceGenerator.Tests/AttributeValueConversion/Types/NumberTests.cs
@@ -77,7 +77,7 @@ public class NumberTests
             .Should()
             .NotBeEmpty()
             .And
-            .ContainKey(nameof(UInt16Class.Count))
+            .ContainKey(nameof(UInt64Class.Count))
             .And
             .ContainSingle(x => x.Value.N == count.ToString());
     }
@@ -157,7 +157,67 @@ public class NumberTests
             .Should()
             .NotBeEmpty()
             .And
-            .ContainKey(nameof(Int16Class.Count))
+            .ContainKey(nameof(Int64Class.Count))
+            .And
+            .ContainSingle(x => x.Value.N == count.ToString());
+    }
+
+    [Fact]
+    public void BuildAttributeValues_DoubleProperty_Included()
+    {
+        const double count = 2.1d;
+        var @class = new DoubleClass
+        {
+            Count = count
+        };
+
+        var result = @class.BuildAttributeValues();
+
+        result
+            .Should()
+            .NotBeEmpty()
+            .And
+            .ContainKey(nameof(DoubleClass.Count))
+            .And
+            .ContainSingle(x => x.Value.N == count.ToString());
+    }
+
+    [Fact]
+    public void BuildAttributeValues_FloatProperty_Included()
+    {
+        const float count = 2.1f;
+        var @class = new FloatClass()
+        {
+            Count = count
+        };
+
+        var result = @class.BuildAttributeValues();
+
+        result
+            .Should()
+            .NotBeEmpty()
+            .And
+            .ContainKey(nameof(FloatClass.Count))
+            .And
+            .ContainSingle(x => x.Value.N == count.ToString());
+    }
+
+    [Fact]
+    public void BuildAttributeValues_DecimalProperty_Included()
+    {
+        const decimal count = 2.1m;
+        var @class = new DecimalClass()
+        {
+            Count = count
+        };
+
+        var result = @class.BuildAttributeValues();
+
+        result
+            .Should()
+            .NotBeEmpty()
+            .And
+            .ContainKey(nameof(DecimalClass.Count))
             .And
             .ContainSingle(x => x.Value.N == count.ToString());
     }
@@ -217,4 +277,25 @@ public partial class UInt64Class
 {
     [DynamoDBProperty]
     public ulong Count { get; set; }
+}
+
+[AttributeValueGenerator]
+public partial class DoubleClass
+{
+    [DynamoDBProperty]
+    public double Count { get; set; }
+}
+
+[AttributeValueGenerator]
+public partial class DecimalClass
+{
+    [DynamoDBProperty]
+    public decimal Count { get; set; }
+}
+
+[AttributeValueGenerator]
+public partial class FloatClass
+{
+    [DynamoDBProperty]
+    public float Count { get; set; }
 }
