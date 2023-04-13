38a1039f1cf43600c18b32a760ce405021940816
Robert Andersson <robert.andersson@mathem.se>
2023-04-13 23:03:54 +0200

    Add more unit test coverage and improve readability of conversion

--- DynamoDBGenerator.SourceGenerator/AttributeValueGenerator.cs
+++ DynamoDBGenerator.SourceGenerator/AttributeValueGenerator.cs
@@ -14,12 +14,45 @@ namespace DynamoDBGenerator.SourceGenerator
     {
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            var putRequestTypes = context.SyntaxProvider
-                .CreateSyntaxProvider(IsAttributeValueAttribute, GetEnumTypeOrNull)
+            var attributeValueGenerators = context.SyntaxProvider
+                .CreateSyntaxProvider((syntaxNode, _) =>
+                    {
+                        if (syntaxNode is not AttributeSyntax attribute)
+                            return false;
+
+                        var name = attribute.Name switch
+                        {
+                            SimpleNameSyntax ins => ins.Identifier.Text,
+                            QualifiedNameSyntax qns => qns.Right.Identifier.Text,
+                            _ => null
+                        };
+
+                        return name switch
+                        {
+                            nameof(AttributeValueGeneratorAttribute) => true,
+                            nameof(AttributeValueGenerator) => true,
+                            _ => false
+                        };
+                    },
+                    (ctx, _) =>
+                    {
+                        var attributeSyntax = (AttributeSyntax) ctx.Node;
+
+                        // "attribute.Parent" is "AttributeListSyntax"
+                        // "attribute.Parent.Parent" is a C# fragment the attributes are applied to
+                        if (attributeSyntax.Parent?.Parent is not ClassDeclarationSyntax classDeclaration)
+                            return null;
+
+                        if (ctx.SemanticModel.GetDeclaredSymbol(classDeclaration) is not ITypeSymbol type)
+                            return null;
+
+                        return IsAttributeValueGenerator(type) is false ? null : type;
+                    }
+                )
                 .Where(x => x is not null)
                 .Collect();
 
-            context.RegisterSourceOutput(putRequestTypes, GenerateCode);
+            context.RegisterSourceOutput(attributeValueGenerators, GenerateCode);
         }
 
         private static void GenerateCode(SourceProductionContext context, ImmutableArray<ITypeSymbol?> typeSymbols)
@@ -38,23 +71,25 @@ namespace DynamoDBGenerator.SourceGenerator
 
                 var generateCode = GenerateCode(type);
 
-                context.AddSource($"{typeNamespace}{nameof(AttributeValueGenerator)}.{type.Name}.g.cs",
-                    SourceText.From(generateCode, Encoding.UTF8));
+                context.AddSource(
+                    $"{typeNamespace}{nameof(AttributeValueGenerator)}.{type.Name}.g.cs",
+                    SourceText.From(generateCode, Encoding.UTF8)
+                );
             }
         }
 
         private static string CreateAttributeValue(ITypeSymbol typeSymbol, string propertyName)
         {
-            static string ConstructList(string propertyName, ITypeSymbol elementType)
+            static string BuildList(string propertyName, ITypeSymbol elementType)
             {
-                var linqAttributeValueSelection = $"Select(x => {CreateAttributeValue(elementType, "x")}))";
+                var select = $"Select(x => {CreateAttributeValue(elementType, "x")}))";
 
                 return elementType.Name is nameof(Nullable)
-                    ? $"L = new List<AttributeValue>({propertyName}.Where(x => x.HasValue).{linqAttributeValueSelection}"
-                    : $"L = new List<AttributeValue>({propertyName}.{linqAttributeValueSelection}";
+                    ? $"L = new List<AttributeValue>({propertyName}.Where(x => x.HasValue).{select}"
+                    : $"L = new List<AttributeValue>({propertyName}.{select}";
             }
 
-            static string? ConstructSet(string propertyName, ITypeSymbol elementType)
+            static string? BuildSet(string propertyName, ITypeSymbol elementType)
             {
                 if (elementType.SpecialType is System_String)
                     return $"SS = new List<string>({propertyName})";
@@ -64,43 +99,52 @@ namespace DynamoDBGenerator.SourceGenerator
                     : $"NS = new List<string>({propertyName}.Select(x => x.ToString()))";
             }
 
-            static bool IsNumeric(ITypeSymbol typeSymbol)
+            static bool IsNumeric(ITypeSymbol typeSymbol) => typeSymbol.SpecialType
+                is System_Int16 or System_Byte
+                or System_Int32 or System_Int64
+                or System_SByte or System_UInt16
+                or System_UInt32 or System_UInt64
+                or System_Decimal or System_Double
+                or System_Single;
+
+            static string? SingleGenericTypeOrNull(ITypeSymbol genericType, string propAccess)
             {
-                return typeSymbol.SpecialType is System_Int16
-                    or System_Byte
-                    or System_Int32 or System_Int64
-                    or System_SByte or System_UInt16
-                    or System_UInt32 or System_UInt64;
+                if (genericType is not INamedTypeSymbol type)
+                    return null;
+
+                if (type is not {IsGenericType: true, TypeArguments.Length: 1})
+                    return null;
+
+                var T = type.TypeArguments[0];
+
+                return type switch
+                {
+                    {Name: nameof(Nullable)} => $"{CreateAssignment(T, $"{propAccess}.Value")}",
+                    _ when type.AllInterfaces.Any(x => x.Name is "ISet") => BuildSet(propAccess, T),
+                    _ when type.AllInterfaces.Any(x => x.Name is nameof(IEnumerable)) => BuildList(propAccess, T),
+                    _ => null
+                };
             }
 
-            static string CreateAssignment(ITypeSymbol typeSymbol, string propertyName)
+            static string? TimeStampOrNull(ITypeSymbol symbol, string propertyAccessor)
+            {
+                return symbol is {SpecialType: System_DateTime} or {Name: nameof(DateTimeOffset) or "DateOnly"}
+                    ? $@"S = {propertyAccessor}.ToString(""O"")"
+                    : null;
+            }
+
+            static string CreateAssignment(ITypeSymbol typeSymbol, string propertyAccessor)
             {
                 return typeSymbol switch
                 {
-                    {SpecialType: System_String} => $"S = {propertyName}",
-                    {SpecialType: System_Boolean} => $"BOOL = {propertyName}",
-                    not null when IsNumeric(typeSymbol) => $"N = {propertyName}.ToString()",
-                    {SpecialType: System_DateTime} or {Name: nameof(DateTimeOffset) or "DateOnly"} =>
-                        $@"S = {propertyName}.ToString(""O"")",
-                    not null when HasAttributeValueGeneratorAttribute(typeSymbol) =>
-                        $"M = {propertyName}.BuildAttributeValues()",
-                    INamedTypeSymbol
-                    {
-                        IsGenericType: true, TypeArguments.Length: 1
-                    } singleGenericType => singleGenericType switch
-                    {
-                        not null when singleGenericType.Name == nameof(Nullable) =>
-                            $"{CreateAssignment(singleGenericType.TypeArguments[0], $"{propertyName}.Value")}",
-                        not null when singleGenericType.AllInterfaces.Any(x => x.Name is "ISet") &&
-                                      ConstructSet(propertyName, singleGenericType.TypeArguments[0]) is { } set => set,
-                        // Is IEnumerable implementation
-                        not null when singleGenericType.AllInterfaces.Any(x => x.Name == nameof(IEnumerable)) =>
-                            ConstructList(propertyName, singleGenericType.TypeArguments[0]),
-                        _ => throw new NotSupportedException(
-                            $"Could not generate AttributeValue for single generic type '{singleGenericType};")
-                    },
-                    IArrayTypeSymbol {ElementType: { } elementType} => ConstructList(propertyName, elementType),
-                    _ => throw new NotSupportedException($"Could not generate AttributeValue for '{typeSymbol};")
+                    {SpecialType: System_String} => $"S = {propertyAccessor}",
+                    {SpecialType: System_Boolean} => $"BOOL = {propertyAccessor}",
+                    _ when IsNumeric(typeSymbol) => $"N = {propertyAccessor}.ToString()",
+                    _ when TimeStampOrNull(typeSymbol, propertyAccessor) is { } assignment => assignment,
+                    _ when IsAttributeValueGenerator(typeSymbol) => $"M = {propertyAccessor}.BuildAttributeValues()",
+                    IArrayTypeSymbol {ElementType: { } elementType} => BuildList(propertyAccessor, elementType),
+                    _ when SingleGenericTypeOrNull(typeSymbol, propertyAccessor) is { } assignment => assignment,
+                    _ => throw new NotSupportedException($"Could not generate AttributeValue for '{typeSymbol}'.")
                 };
             }
 
@@ -109,7 +153,8 @@ namespace DynamoDBGenerator.SourceGenerator
 
         private static string BuildAttributeDictionaryMethod(string methodName, ITypeSymbol type)
         {
-            const string dictionaryName = $"attributeValues";
+            const string dictionaryName = "attributeValues";
+            const string indent = "    ";
             var assignments = GetDynamoDbProperties(type).Select(x =>
                 {
                     var add = @$"{dictionaryName}.Add(""{x.Name}"", {CreateAttributeValue(x.Type, x.Name)});";
@@ -117,16 +162,17 @@ namespace DynamoDBGenerator.SourceGenerator
                         ? @$"if ({x.Name} != default) {{ {add} }} "
                         : add;
                 })
+                .Select(x => $"{indent}{x}")
                 .ToArray();
 
             return @$"
 public Dictionary<string, AttributeValue> {methodName}()
-  {{ 
-      var {dictionaryName} = new Dictionary<string, AttributeValue>(capacity: {assignments.Length});
-      {string.Join(Constants.NewLine, assignments)}
+{{ 
+    var {dictionaryName} = new Dictionary<string, AttributeValue>(capacity: {assignments.Length});
+{string.Join(Constants.NewLine, assignments)}
 
-       return {dictionaryName};
-  }}";
+    return {dictionaryName};
+}}";
         }
 
         private static string GenerateCode(ITypeSymbol type)
@@ -135,7 +181,6 @@ public Dictionary<string, AttributeValue> {methodName}()
                 ? null
                 : type.ContainingNamespace.ToString();
             var name = type.Name;
-            // TODO add class with consts called 'AttribueValueKeys'
 
             return @$"// <auto-generated />
 
@@ -161,7 +206,7 @@ using System.Linq;
         }
 
 
-        private static bool IsAttributeValueAttribute(
+        private static bool IsAttributeValueGenerator(
             SyntaxNode syntaxNode,
             CancellationToken cancellationToken)
         {
@@ -183,24 +228,7 @@ using System.Linq;
             };
         }
 
-        private static ITypeSymbol? GetEnumTypeOrNull(
-            GeneratorSyntaxContext context,
-            CancellationToken cancellationToken)
-        {
-            var attributeSyntax = (AttributeSyntax) context.Node;
-
-            // "attribute.Parent" is "AttributeListSyntax"
-            // "attribute.Parent.Parent" is a C# fragment the attributes are applied to
-            if (attributeSyntax.Parent?.Parent is not ClassDeclarationSyntax classDeclaration)
-                return null;
-
-            if (context.SemanticModel.GetDeclaredSymbol(classDeclaration) is not ITypeSymbol type)
-                return null;
-
-            return HasAttributeValueGeneratorAttribute(type) is false ? null : type;
-        }
-
-        private static bool HasAttributeValueGeneratorAttribute(ISymbol type)
+        private static bool IsAttributeValueGenerator(ISymbol type)
         {
             return type
                 .GetAttributes()
