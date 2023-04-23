eaa21e3fc4c2700ca0dbf28e1592d46851569564
Robert Andersson <robert.andersson@mathem.se>
2023-04-23 02:13:06 +0200

    Generate properties that not marked with DynamoDbIgnore

--- DynamoDBGenerator.SourceGenerator/IEnumerableExtensions.cs
+++ DynamoDBGenerator.SourceGenerator/IEnumerableExtensions.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using Amazon.DynamoDBv2.DataModel;
+using Microsoft.CodeAnalysis;
 
 namespace DynamoDBGenerator.SourceGenerator;
 
 public static class EnumerableExtensions
 {
+    public static IEnumerable<IPropertySymbol> GetDynamoDbProperties(this INamespaceOrTypeSymbol type)
+    {
+        return type
+            .GetPublicInstanceProperties()
+            .Where(x =>
+            {
+                var attributes = x.GetAttributes();
+                if (attributes.Length == 0)
+                    return true;
+
+                return attributes
+                    .Any(y => y.AttributeClass is not
+                    {
+                        Name: nameof(DynamoDBIgnoreAttribute),
+                        ContainingNamespace.Name: nameof(Amazon.DynamoDBv2.DataModel)
+                    });
+            });
+    }
+
     public static IEnumerable<IPropertySymbol> GetPublicInstanceProperties(this INamespaceOrTypeSymbol symbol)
     {
         return symbol
@@ -13,16 +33,5 @@ public static class EnumerableExtensions
             .Where(x => x is not null)
             .Where(x => x is IPropertySymbol)
             .Cast<IPropertySymbol>();
-    }
-
-    public static IEnumerable<IMethodSymbol> GetPublicInstanceMethods(this INamespaceOrTypeSymbol symbol)
-    {
-        return symbol
-            .GetMembers()
-            .Where(x => x.IsStatic is false)
-            .Where(x => x.DeclaredAccessibility == Accessibility.Public)
-            .Where(x => x is not null)
-            .Where(x => x is IMethodSymbol)
-            .Cast<IMethodSymbol>();
     }
 }
