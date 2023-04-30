8bde95640458cfdec639aba23f70f598c13f4ccb
Robert Andersson <robert.andersson@mathem.se>
2023-05-01 01:45:55 +0200

    Use `in` keyword to reuse references

--- DynamoDBGenerator.SourceGenerator/Extensions/CodeGeneration/AttributeValue/AttributeValueConversion.cs
+++ DynamoDBGenerator.SourceGenerator/Extensions/CodeGeneration/AttributeValue/AttributeValueConversion.cs
@@ -5,9 +5,12 @@ namespace DynamoDBGenerator.SourceGenerator.Extensions.CodeGeneration.AttributeV
 
 public static class AttributeValueConversion
 {
-    public static AttributeValueAssignment CreateAttributeValue(ITypeSymbol typeSymbol, string accessPattern)
+    public static AttributeValueAssignment CreateAttributeValue(
+        in ITypeSymbol typeSymbol,
+        in string accessPattern
+    )
     {
-        static AttributeValueAssignment BuildList(ITypeSymbol elementType, string accessPattern)
+        static AttributeValueAssignment BuildList(in ITypeSymbol elementType, in string accessPattern)
         {
             var attributeValue = CreateAttributeValue(elementType, "x");
             var select = $"Select(x => {attributeValue}))";
@@ -18,14 +21,15 @@ public static class AttributeValueConversion
             return new AttributeValueAssignment(in assignment, in elementType, attributeValue.AssignedBy);
         }
 
-        static AttributeValueAssignment? BuildSet(ITypeSymbol elementType, string accessPattern)
+        static AttributeValueAssignment? BuildSet(in ITypeSymbol elementType, in string accessPattern)
         {
-            if (elementType.LambdaExpression() is { } lambdaExpression)
-                accessPattern = $"{accessPattern}.Where({lambdaExpression})";
+            var newAccessPattern = elementType.LambdaExpression() is { } expression
+                ? $"{accessPattern}.Where({expression})"
+                : accessPattern;
 
             if (elementType.SpecialType is SpecialType.System_String)
                 return new AttributeValueAssignment(
-                    $"SS = new List<string>({accessPattern})",
+                    $"SS = new List<string>({newAccessPattern})",
                     in elementType,
                     AttributeValueAssignment.Decision.Inline
                 );
@@ -33,13 +37,13 @@ public static class AttributeValueConversion
             return IsNumeric(elementType) is false
                 ? null
                 : new AttributeValueAssignment(
-                    $"NS = new List<string>({accessPattern}.Select(x => x.ToString()))",
+                    $"NS = new List<string>({newAccessPattern}.Select(x => x.ToString()))",
                     in elementType,
                     AttributeValueAssignment.Decision.Inline
                 );
         }
 
-        static bool IsNumeric(ITypeSymbol typeSymbol) => typeSymbol.SpecialType
+        static bool IsNumeric(in ITypeSymbol typeSymbol) => typeSymbol.SpecialType
             is SpecialType.System_Int16 or SpecialType.System_Byte
             or SpecialType.System_Int32 or SpecialType.System_Int64
             or SpecialType.System_SByte or SpecialType.System_UInt16
@@ -47,7 +51,7 @@ public static class AttributeValueConversion
             or SpecialType.System_Decimal or SpecialType.System_Double
             or SpecialType.System_Single;
 
-        static AttributeValueAssignment? SingleGenericTypeOrNull(ITypeSymbol genericType, string accessPattern)
+        static AttributeValueAssignment? SingleGenericTypeOrNull(in ITypeSymbol genericType, in string accessPattern)
         {
             if (genericType is not INamedTypeSymbol type)
                 return null;
@@ -59,16 +63,17 @@ public static class AttributeValueConversion
 
             return type switch
             {
-                {Name: nameof(Nullable)} => CreateAssignment(T, $"{accessPattern}.Value"),
-                {Name: "ISet"} => BuildSet(T, accessPattern),
-                {Name: nameof(IEnumerable)} => BuildList(T, accessPattern),
-                _ when type.AllInterfaces.Any(x => x is {Name: "ISet"}) => BuildSet(T, accessPattern),
-                _ when type.AllInterfaces.Any(x => x is {Name: nameof(IEnumerable)}) => BuildList(T, accessPattern),
+                {Name: nameof(Nullable)} => CreateAssignment(in T, $"{accessPattern}.Value"),
+                {Name: "ISet"} => BuildSet(in T, in accessPattern),
+                {Name: nameof(IEnumerable)} => BuildList(in T, in accessPattern),
+                _ when type.AllInterfaces.Any(x => x is {Name: "ISet"}) => BuildSet(in T, in accessPattern),
+                _ when type.AllInterfaces.Any(x => x is {Name: nameof(IEnumerable)}) => BuildList(in T,
+                    in accessPattern),
                 _ => null
             };
         }
 
-        static AttributeValueAssignment? DoubleGenericTypeOrNull(ITypeSymbol genericType, string accessPattern)
+        static AttributeValueAssignment? DoubleGenericTypeOrNull(in ITypeSymbol genericType, in string accessPattern)
         {
             if (genericType is not INamedTypeSymbol type)
                 return null;
@@ -86,28 +91,28 @@ public static class AttributeValueConversion
                 case var _ when T1.SpecialType is not SpecialType.System_String:
                     return null;
                 case {Name: "ILookup"}:
-                    var lookupValueList = BuildList(T2, "x");
+                    var lookupValueList = BuildList(in T2, "x");
                     return new AttributeValueAssignment(
                         $"M = {accessPattern}.ToDictionary(x => x.Key, x => {lookupValueList})",
                         in T2,
                         lookupValueList.AssignedBy
                     );
                 case {Name: "IGrouping"}:
-                    var groupingValueList = BuildList(T2, accessPattern);
+                    var groupingValueList = BuildList(in T2, accessPattern);
                     return new AttributeValueAssignment(
                         $@"M = new Dictionary<string, AttributeValue>{{ {{ {accessPattern}.Key, {groupingValueList}}} }}",
                         in T2,
                         groupingValueList.AssignedBy
                     );
                 case {Name: "Dictionary" or "IReadOnlyDictionary" or "IDictionary"}:
-                    var dictionary = CreateAttributeValue(T2, "x.Value");
+                    var dictionary = CreateAttributeValue(in T2, "x.Value");
                     return new AttributeValueAssignment(
                         $@"M = {accessPattern}.ToDictionary(x => x.Key, x => {dictionary})",
                         in T2,
                         dictionary.AssignedBy
                     );
                 case {Name: "KeyValuePair"}:
-                    var keyValuePair = CreateAttributeValue(T2, $"{accessPattern}.Value");
+                    var keyValuePair = CreateAttributeValue(in T2, $"{accessPattern}.Value");
                     return new AttributeValueAssignment(
                         $@"M = new Dictionary<string, AttributeValue>() {{ {{{accessPattern}.Key, {keyValuePair} }} }}",
                         in T2,
@@ -119,19 +124,19 @@ public static class AttributeValueConversion
         }
 
 
-        static bool IsTimeRelated(ITypeSymbol symbol)
+        static bool IsTimeRelated(in ITypeSymbol symbol)
         {
             return symbol is {SpecialType: SpecialType.System_DateTime} or {Name: nameof(DateTimeOffset) or "DateOnly"};
         }
 
-        static AttributeValueAssignment CreateAssignment(ITypeSymbol typeSymbol, string accessPattern)
+        static AttributeValueAssignment CreateAssignment(in ITypeSymbol typeSymbol, in string accessPattern)
         {
             var baseTypeConversion = typeSymbol switch
             {
                 {SpecialType: SpecialType.System_String} => $"S = {accessPattern}",
                 {SpecialType: SpecialType.System_Boolean} => $"BOOL = {accessPattern}",
-                _ when IsNumeric(typeSymbol) => $"N = {accessPattern}.ToString()",
-                _ when IsTimeRelated(typeSymbol) => $@"S = {accessPattern}.ToString(""O"")",
+                _ when IsNumeric(in typeSymbol) => $"N = {accessPattern}.ToString()",
+                _ when IsTimeRelated(in typeSymbol) => $@"S = {accessPattern}.ToString(""O"")",
                 _ => null
             };
 
@@ -144,9 +149,9 @@ public static class AttributeValueConversion
 
             AttributeValueAssignment? genericConversion = typeSymbol switch
             {
-                _ when SingleGenericTypeOrNull(typeSymbol, accessPattern) is { } assignment => assignment,
-                _ when DoubleGenericTypeOrNull(typeSymbol, accessPattern) is { } assignment => assignment,
-                IArrayTypeSymbol {ElementType: { } elementType} => BuildList(elementType, accessPattern),
+                _ when SingleGenericTypeOrNull(in typeSymbol, in accessPattern) is { } assignment => assignment,
+                _ when DoubleGenericTypeOrNull(in typeSymbol, in accessPattern) is { } assignment => assignment,
+                IArrayTypeSymbol {ElementType: { } elementType} => BuildList(in elementType, in accessPattern),
                 _ => null
             };
 
