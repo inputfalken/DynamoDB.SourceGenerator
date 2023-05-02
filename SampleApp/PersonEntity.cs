ffa3b20656a00254b583665efa57090af73949f6
Robert Andersson <robert.andersson@mathem.se>
2023-05-02 23:12:20 +0200

    Revert changes in SampleApp

--- SampleApp/PersonEntity.cs
+++ SampleApp/PersonEntity.cs
@@ -1,11 +1,23 @@
-﻿using DynamoDBGenerator;
+﻿using System.ComponentModel;
+using Amazon.DynamoDBv2.DataModel;
+using DynamoDBGenerator;
 
 namespace SampleApp;
 
 [AttributeValueGenerator("Abc")]
 public partial class PersonEntity
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public IEnumerable<KeyValuePair<string, int>> Type { get; set; } = null!;
 
-    public (int X, int Y, int Z) Coordinate { get; set; }
+    [DynamoDBHashKey]
+    public string MyHashKey { get; set; } = null!;
 
+    public PersonEntity()
+    
+    {
+    }
+    public class Testing
+    {
+    }
 }
