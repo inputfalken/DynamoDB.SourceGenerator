1acf57ddce343ecc6789dd15414ee3c002a1d761
inputfalken <inputfalken@gmail.com>
2023-07-13 23:07:42 +0200

    Add tests to verify parameter works

--- DynamoDBGenerator.SourceGenerator.Tests/DynamoDBDocument/ConditionExpressionTests.cs
+++ DynamoDBGenerator.SourceGenerator.Tests/DynamoDBDocument/ConditionExpressionTests.cs
@@ -1,30 +1,62 @@
 namespace DynamoDBGenerator.SourceGenerator.Tests.DynamoDBDocument;
 
-[DynamoDbDocumentProperty(typeof(MainClass))]
+/// <summary>
+/// NOTE the order of the assertions are intended to prove that the order you access the properties should not change their underlying value.
+/// </summary>
+[DynamoDbDocumentProperty(typeof(TripleNestedClass))]
+[DynamoDbDocumentProperty(typeof(FlatClass))]
 public partial class AttributeExpressionTests
 {
     [Fact]
-    public void Value_References_AreDeterministic()
+    public void TripleNestedClass_AttributeReferences_ShouldBeCorrectlySet()
     {
-        var references = new MainClass_Document.MainClassReferences(default, default);
+        var references = new TripleNestedClass_Document.TripleNestedClassReferences(null, 0);
 
         references.ThreeToSix.FiveToSix.Five.Value.Should().Be(":p5");
+        references.ThreeToSix.FiveToSix.Five.Name.Should().Be("#ThreeToSix.#FiveToSix.#Five");
+        
         references.SevenToEight.Seven.Value.Should().Be(":p7");
+        references.SevenToEight.Seven.Name.Should().Be("#SevenToEight.#Seven");
+        
+        references.Nine.Value.Should().Be(":p9");
+        references.Nine.Name.Should().Be("#Nine");
+        
+        references.Two.Value.Should().Be(":p2");
+        references.Two.Name.Should().Be("#Two");
+        
+        references.One.Value.Should().Be(":p1");
+        references.One.Name.Should().Be("#One");
+        
+        references.ThreeToSix.Three.Value.Should().Be(":p3");
+        references.ThreeToSix.Three.Name.Should().Be("#ThreeToSix.#Three");
 
     }
-
+    
     [Fact]
-    public void Test()
+    public void FlatClass_AttributeReferences_ShouldBeCorrectlySet()
     {
-        MainClassDocument
-            .ConditionExpression(x => $"{x.SevenToEight.Seven.Value} <> {x.SevenToEight.Seven.Name}").Expression
-            .Should()
-            .Be($":p7 <> #{nameof(MainClass.SevenToEight)}.#{nameof(MainClass.SubClassTwo.Seven)}");
-    }
+        var references = new FlatClass_Document.FlatClassReferences(null, 0);
 
+        references.Two.Value.Should().Be(":p2");
+        references.Two.Name.Should().Be("#Two");
+        
+        references.One.Value.Should().Be(":p1");
+        references.One.Name.Should().Be("#One");
+        
+        references.Three.Value.Should().Be(":p3");
+        references.Three.Name.Should().Be("#Three");
+
+    }
 }
 
-public class MainClass
+public class FlatClass
+{
+    public string One { get; set; }
+    public string Two { get; set; }
+    public string Three { get; set; }
+}
+
+public class TripleNestedClass
 {
     public string One { get; set; }
     public string Two { get; set; }
