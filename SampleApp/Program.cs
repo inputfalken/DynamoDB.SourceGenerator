862c866533a9dbcd97113b167108011d77c66ca9
inputfalken <inputfalken@gmail.com>
2023-07-13 23:42:51 +0200

    Remove redundant code in sampleapp

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,14 +1,4 @@
-﻿using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-using System.Text.Json.Serialization;
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.Model;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
-using BenchmarkDotNet.Running;
-using DynamoDBGenerator;
-using DynamoDBGenerator.Extensions;
+﻿using DynamoDBGenerator;
 
 namespace SampleApp;
 
@@ -16,8 +6,6 @@ internal static class Program
 {
     public static void Main()
     {
-        BenchmarkRunner.Run<ExpressionBuilder>();
-
         var person = new PersonEntity()
         {
             Id = "Abc",
@@ -37,31 +25,10 @@ internal static class Program
 
         };
 
-        var repo = new Repository();
-
-        for (int i = 0; i < 100; i++)
-        {
-            var stopwatch = Stopwatch.StartNew();
-
-            var lazy = new Lazy<string>("");
-            PutItemRequest update = repo.PersonEntityDocument.ToPutItemRequest(person, BuildConditionExpression);
-            Console.WriteLine(stopwatch.Elapsed);
-            repo.PersonEntityDocument.ToPutItemRequest()
-            stopwatch.Restart();
-        }
-    }
-    private static string BuildConditionExpression(Repository.PersonEntity_Document.PersonEntityReferences x)
-    {
-        return $"{x.Address.PostalCode.Town.Name} <> {x.Address.PostalCode.Town.Value}";
     }
 
 }
 
-public class Person
-{
-    public string FirstName { get; set; }
-    public IReadOnlyList<Person> Friends { get; set; }
-}
 
 [DynamoDbDocumentProperty(typeof(PersonEntity))]
 [DynamoDbDocumentProperty(typeof(Address))]
@@ -70,16 +37,4 @@ public partial class Repository
     public Repository()
     {
     }
-}
-
-[SimpleJob]
-[MemoryDiagnoser]
-public class ExpressionBuilder
-{
-    private readonly IDynamoDbDocument<PersonEntity, Repository.PersonEntity_Document.PersonEntityReferences> _repository = new Repository().PersonEntityDocument;
-
-    [Benchmark]
-    public string SingleCondition() => _repository.UpdateExpression(x => $"{x.Address.PostalCode.Town.Name} <> {x.Address.PostalCode.Town.Value}").Expression;
-    [Benchmark]
-    public string DoubleCondition() => _repository.UpdateExpression(x => $"SET {x.Address.PostalCode.Town.Name} <> {x.Address.PostalCode.Town.Value} AND {x.Address.PostalCode.ZipCode.Name} <> {x.Address.PostalCode.ZipCode.Value}").Expression;
 }
