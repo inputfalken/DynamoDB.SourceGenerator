ac78bdd9cbd99242acad2f7475e202aa8fbad4de
inputfalken <inputfalken@gmail.com>
2023-08-08 22:27:37 +0200

    Add lots of unit tests

---
+++ DynamoDBGenerator.SourceGenerator.Tests/DynamoDBDocumentTests/Deserialize/Generics/LookUpTests.cs
@@ -0,0 +1,59 @@
+using Amazon.DynamoDBv2.Model;
+namespace DynamoDBGenerator.SourceGenerator.Tests.DynamoDBDocumentTests.Deserialize.Generics;
+
+[DynamoDBDocument(typeof(LookUpClass))]
+public partial class LookUpTests
+{
+    [Fact]
+    public void Deserialize_EmptyLookup_IsIncluded()
+    {
+        LookUpClassDocument
+            .Deserialize(new Dictionary<string, AttributeValue> {{nameof(LookUpClass.Lookup), new AttributeValue {M = new Dictionary<string, AttributeValue>()}}})
+            .Should()
+            .BeOfType<LookUpClass>()
+            .Which
+            .Lookup
+            .Should()
+            .BeEmpty();
+    }
+
+    [Fact]
+    public void Deserialize_LookupWithValues_IsIncluded()
+    {
+        LookUpClassDocument
+            .Deserialize(new Dictionary<string, AttributeValue>
+            {
+                {
+                    nameof(LookUpClass.Lookup), new AttributeValue
+                    {
+                        M = new Dictionary<string, AttributeValue>
+                        {
+                            {"first", new AttributeValue {L = new List<AttributeValue> {new() {N = "1"}, new() {N = "2"}}}},
+                            {"second", new AttributeValue {L = new List<AttributeValue> {new() {N = "3"}, new() {N = "4"}}}}
+                        }
+                    }
+                }
+            })
+            .Should()
+            .BeOfType<LookUpClass>()
+            .Which
+            .Lookup
+            .Should()
+            .SatisfyRespectively(x =>
+            {
+                x.Key.Should().Be("first");
+                x.Should().SatisfyRespectively(y => y.Should().Be(1), y => y.Should().Be(2));
+            }, x =>
+            {
+
+                x.Key.Should().Be("second");
+                x.Should().SatisfyRespectively(y => y.Should().Be(3), y => y.Should().Be(4));
+            });
+    }
+}
+
+public class LookUpClass
+{
+    [DynamoDBProperty]
+    public ILookup<string, int>? Lookup { get; set; }
+}
---
+++ DynamoDBGenerator.SourceGenerator.Tests/DynamoDBDocumentTests/Deserialize/PropertyRenamingTests.cs
@@ -0,0 +1,56 @@
+using Amazon.DynamoDBv2.Model;
+namespace DynamoDBGenerator.SourceGenerator.Tests.DynamoDBDocumentTests.Deserialize;
+
+[DynamoDBDocument(typeof(PropertyWithMixedNames))]
+public partial class PropertyRenamingTests
+{
+    [Fact]
+    public void Serialize_Attributes_ChangesNames()
+    {
+
+        var propertyAssertion = PropertyWithMixedNamesDocument
+            .Deserialize(new Dictionary<string, AttributeValue>
+            {
+                {nameof(PropertyWithMixedNames.PlainProperty), new AttributeValue {S = "1"}},
+                {nameof(PropertyWithMixedNames.PropertyWithDdbPropertyAttribute), new AttributeValue {S = "2"}},
+                {"AnotherName", new AttributeValue {S = "3"}},
+                {nameof(PropertyWithMixedNames.PropertyWithDdbHashKeyAttribute), new AttributeValue {S = "4"}},
+                {"AnotherHashKey", new AttributeValue {S = "5"}},
+                {nameof(PropertyWithMixedNames.PropertyWithDdbRangeKeyAttribute), new AttributeValue {S = "6"}},
+                {"AnotherRangeKey", new AttributeValue {S = "7"}}
+            })
+            .Should()
+            .BeOfType<PropertyWithMixedNames>();
+
+        propertyAssertion.Which.PlainProperty.Should().Be("1");
+        propertyAssertion.Which.PropertyWithDdbPropertyAttribute.Should().Be("2");
+        propertyAssertion.Which.PropertyWithDdbPropertyAttributeWithOverridenName.Should().Be("3");
+        propertyAssertion.Which.PropertyWithDdbHashKeyAttribute.Should().Be("4");
+        propertyAssertion.Which.PropertyWithDdbHashKeyAttributeWithOverridenName.Should().Be("5");
+        propertyAssertion.Which.PropertyWithDdbRangeKeyAttribute.Should().Be("6");
+        propertyAssertion.Which.PropertyWithDdbRangeKeyAttributeWithOverridenName.Should().Be("7");
+    }
+}
+
+public class PropertyWithMixedNames
+{
+    public string PlainProperty { get; init; } = null!;
+
+    [DynamoDBProperty]
+    public string PropertyWithDdbPropertyAttribute { get; init; } = null!;
+
+    [DynamoDBProperty("AnotherName")]
+    public string PropertyWithDdbPropertyAttributeWithOverridenName { get; init; } = null!;
+
+    [DynamoDBHashKey]
+    public string PropertyWithDdbHashKeyAttribute { get; init; } = null!;
+
+    [DynamoDBHashKey("AnotherHashKey")]
+    public string PropertyWithDdbHashKeyAttributeWithOverridenName { get; init; } = null!;
+
+    [DynamoDBRangeKey]
+    public string PropertyWithDdbRangeKeyAttribute { get; init; } = null!;
+
+    [DynamoDBRangeKey("AnotherRangeKey")]
+    public string PropertyWithDdbRangeKeyAttributeWithOverridenName { get; init; } = null!;
+}
