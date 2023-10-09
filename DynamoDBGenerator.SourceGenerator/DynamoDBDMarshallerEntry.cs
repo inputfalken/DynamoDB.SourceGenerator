c354e2658b495322aa5f7adea8b6a2bd250cae3b
inputfalken <inputfalken@gmail.com>
2023-10-09 21:42:36 +0200

    Inline redundant method

--- DynamoDBGenerator.SourceGenerator/DynamoDBDMarshallerEntry.cs
+++ DynamoDBGenerator.SourceGenerator/DynamoDBDMarshallerEntry.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
-using System.Linq;
 using DynamoDBGenerator.Attributes;
 using DynamoDBGenerator.SourceGenerator.Extensions;
 using DynamoDBGenerator.SourceGenerator.Types;
@@ -39,7 +36,10 @@ public class DynamoDBDMarshallerEntry : IIncrementalGenerator
         foreach (var typeSymbol in compilation.GetTypeSymbols(documents))
         {
             var timestamp = Stopwatch.GetTimestamp();
-            var repository = string.Join(Constants.NewLine, GetMethods(typeSymbol, compilation));
+            var repository = string.Join(
+                Constants.NewLine,
+                CreateArguments(typeSymbol, compilation).Select(x => new DynamoDbMarshaller(x).CreateDynamoDbDocumentProperty(Accessibility.Public))
+            );
             var code = typeSymbol.CreateNamespace(typeSymbol.CreateClass(repository), TimeSpan.FromTicks(Stopwatch.GetTimestamp() - timestamp));
             var typeNamespace = typeSymbol.ContainingNamespace.IsGlobalNamespace
                 ? null
@@ -49,7 +49,12 @@ public class DynamoDBDMarshallerEntry : IIncrementalGenerator
     }
 
 
-    private static IEnumerable<string> GetMethods(ISymbol typeSymbol, Compilation compilation)
+    // If we can find duplicated types, we could access their properties instead of duplicating the source generator. 
+    // For example :
+    // [DynamoDBDocument(typeof(Person))]
+    // [DynamoDBDocument(typeof(Person), ArgumentType = typeof(ChangeName))]
+    // With this scenario we would be able to use the Person type from the second attribute instead of source generating duplicated code.
+    private static IEnumerable<DynamoDBMarshallerArguments> CreateArguments(ISymbol typeSymbol, Compilation compilation)
     {
         var attributes = typeSymbol
             .GetAttributes()
@@ -60,17 +65,6 @@ public class DynamoDBDMarshallerEntry : IIncrementalGenerator
                 ContainingAssembly.Name: Constants.AssemblyName
             });
 
-        return CreateArguments(attributes, compilation)
-            .Select(x => new DynamoDbMarshaller(x).CreateDynamoDbDocumentProperty(Accessibility.Public));
-    }
-
-    // If we can find duplicated types, we could access their properties instead of duplicating the source generator. 
-    // For example :
-    // [DynamoDBDocument(typeof(Person))]
-    // [DynamoDBDocument(typeof(Person), ArgumentType = typeof(ChangeName))]
-    // With this scenario we would be able to use the Person type from the second attribute instead of source generating duplicated code.
-    private static IEnumerable<DynamoDBMarshallerArguments> CreateArguments(IEnumerable<AttributeData> attributes, Compilation compilation)
-    {
         foreach (var attributeData in attributes)
         {
             var entityType = attributeData.ConstructorArguments
