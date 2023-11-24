77d558f2df73c87152b385ac760ec243db2053bc
inputfalken <inputfalken@gmail.com>
2023-11-24 10:49:09 +0100

    Reset samples

--- samples/ConsoleApp/PersonEntity.cs
+++ samples/ConsoleApp/PersonEntity.cs
@@ -6,9 +6,7 @@ public class PersonEntity
     [DynamoDBHashKey]
     public string Id { get; set; }
 
-    [DynamoDBLocalSecondaryIndexRangeKey(new []{"FOo"})]
     public string Firstname { get; set; }
-    [DynamoDBGlobalSecondaryIndexHashKey(new []{"abc"})]
     public string Lastname { get; set; }
     public Address Address { get; set; }
 }
