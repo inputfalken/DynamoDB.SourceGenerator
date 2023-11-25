f37e6541120110a54d5b91b983cbfc804176ce2d
Robert Andersson <robert.andersson@mathem.se>
2023-11-25 21:31:57 +0100

    Support inheritance (#52)

--- src/DynamoDBGenerator.SourceGenerator/Extensions/EnumerableExtensions.cs
+++ src/DynamoDBGenerator.SourceGenerator/Extensions/EnumerableExtensions.cs
@@ -5,22 +5,26 @@ namespace DynamoDBGenerator.SourceGenerator.Extensions;
 
 public static class EnumerableExtensions
 {
-    public static IEnumerable<DynamoDbDataMember> GetDynamoDbProperties(this INamespaceOrTypeSymbol type)
+    public static IEnumerable<DynamoDbDataMember> GetDynamoDbProperties(this ITypeSymbol type)
     {
         // ReSharper disable once LoopCanBeConvertedToQuery
         foreach (var publicInstanceProperty in type.GetPublicInstanceProperties())
-            yield return new DynamoDbDataMember(publicInstanceProperty);
+            yield return new DynamoDbDataMember(in publicInstanceProperty);
     }
-    
-    private static IEnumerable<DataMember> GetPublicInstanceProperties(this INamespaceOrTypeSymbol symbol)
+
+    private static IEnumerable<DataMember> GetPublicInstanceProperties(this ITypeSymbol symbol)
     {
+
+        if (symbol.BaseType is {SpecialType: not SpecialType.System_Object})
+            foreach (var publicInstanceProperty in GetPublicInstanceProperties(symbol.BaseType))
+                yield return publicInstanceProperty;
+
         var publicInstanceDataMembers = symbol
             .GetMembers()
             .Where(x => x.IsStatic is false)
             .Where(x => x.DeclaredAccessibility == Accessibility.Public)
             .Where(x => x.Kind is SymbolKind.Field or SymbolKind.Property)
             .Where(x => x.CanBeReferencedByName);
-
         // A special rule when it comes to Tuples.
         // If we remove this we will get duplicated DataMembers when tuples are being used.
         if (symbol is INamedTypeSymbol {IsTupleType: true} namedTypeSymbol)
@@ -36,9 +40,11 @@ public static class EnumerableExtensions
             {
                 case IPropertySymbol propertySymbol:
                     yield return DataMember.FromProperty(in propertySymbol);
+
                     break;
                 case IFieldSymbol fieldSymbol:
                     yield return DataMember.FromField(in fieldSymbol);
+
                     break;
             }
     }
---
+++ tests/DynamoDBGenerator.SourceGenerator.Tests/DynamoDBDocumentTests/Serialize/InheritanceTests.cs
@@ -0,0 +1,40 @@
+using DynamoDBGenerator.Attributes;
+namespace DynamoDBGenerator.SourceGenerator.Tests.DynamoDBDocumentTests.Serialize;
+
+[DynamoDBMarshaller(typeof(Cat))]
+public partial class InheritanceTests
+{
+    [Fact]
+    public void Includes_Inherited_DataMember()
+    {
+        var cat = new Cat {Name = "Mjaao", MjaoFactor = 100};
+
+        CatMarshaller
+            .Marshall(cat)
+            .Should()
+            .SatisfyRespectively(x =>
+            {
+                x.Key.Should().Be(nameof(cat.Name));
+                x.Value.S.Should().Be("Mjaao");
+            }, x =>
+            {
+
+                x.Key.Should().Be(nameof(cat.MjaoFactor));
+                x.Value.N.Should().Be("100");
+            });
+
+    }
+
+
+}
+
+public class Animal
+{
+    public string Name { get; set; }
+
+}
+
+public class Cat : Animal
+{
+    public int MjaoFactor { get; set; }
+}
