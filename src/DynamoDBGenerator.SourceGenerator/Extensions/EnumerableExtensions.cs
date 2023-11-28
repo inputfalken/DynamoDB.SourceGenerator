1af0de043b4754b7e95cd70e7acccf65726a5821
inputfalken <inputfalken@gmail.com>
2023-11-28 12:12:12 +0100

    Move extensions

--- src/DynamoDBGenerator.SourceGenerator/Extensions/EnumerableExtensions.cs
+++ src/DynamoDBGenerator.SourceGenerator/Extensions/EnumerableExtensions.cs
@@ -8,61 +8,6 @@ namespace DynamoDBGenerator.SourceGenerator.Extensions;
 public static class EnumerableExtensions
 {
 
-    private static readonly IDictionary<int, string> IndentCache = new Dictionary<int, string>
-    {
-        {0, ""},
-        {1, "    "},
-        {2, "        "},
-        {3, "            "},
-        {4, "                "}
-    };
-
-    private static string Indent(int level)
-    {
-        if (IndentCache.TryGetValue(level, out var indent)) return indent;
-
-        indent = new string(' ', level * 4);
-        IndentCache[level] = indent;
-
-        return indent;
-    }
-    public static IEnumerable<string> CreateBlock(this string header, IEnumerable<string> content, int indentLevel) => CreateBlockPrivate(header, content, indentLevel);
-
-    public static IEnumerable<string> CreateBlock(this string header, string content, int indentLevel) => CreateBlockPrivate(header, content, indentLevel);
-
-    private static IEnumerable<string> CreateBlockPrivate(string header, object content, int indentLevel)
-    {
-        if (indentLevel is 0)
-        {
-            yield return header;
-            yield return "{";
-
-            if (content is IEnumerable<string> enumerable)
-                foreach (var s in enumerable)
-                    yield return $"{Indent(1)}{s}";
-            else
-                yield return $"{Indent(1)}{content}";
-
-            yield return "}";
-        }
-        else
-        {
-            var indent = Indent(indentLevel);
-
-            yield return $"{indent}{header}";
-            yield return string.Intern($"{indent}{{");
-
-            if (content is IEnumerable<string> enumerable)
-                foreach (var s in enumerable)
-                    yield return $"{Indent(indentLevel + 1)}{s}";
-            else
-                yield return $"{Indent(indentLevel + 1)}{content}";
-
-            yield return string.Intern($"{indent}}}");
-
-        }
-
-    }
     public static IEnumerable<TResult> DefaultAndLast<T, TResult>(this IEnumerable<T> enumerable, Func<T, TResult> @default, Func<T, TResult> onLast)
     {
         var buffer = default(T);
--- src/DynamoDBGenerator.SourceGenerator/Extensions/StringExtensions.cs
+++ src/DynamoDBGenerator.SourceGenerator/Extensions/StringExtensions.cs
@@ -2,6 +2,62 @@ namespace DynamoDBGenerator.SourceGenerator.Extensions;
 
 public static class StringExtensions
 {
+
+    private static readonly IDictionary<int, string> IndentCache = new Dictionary<int, string>
+    {
+        {0, ""},
+        {1, "    "},
+        {2, "        "},
+        {3, "            "},
+        {4, "                "}
+    };
+
+    private static string Indent(int level)
+    {
+        if (IndentCache.TryGetValue(level, out var indent)) return indent;
+
+        indent = new string(' ', level * 4);
+        IndentCache[level] = indent;
+
+        return indent;
+    }
+    public static IEnumerable<string> CreateBlock(this string header, IEnumerable<string> content, int indentLevel) => CreateBlockPrivate(header, content, indentLevel);
+
+    public static IEnumerable<string> CreateBlock(this string header, string content, int indentLevel) => CreateBlockPrivate(header, content, indentLevel);
+
+    private static IEnumerable<string> CreateBlockPrivate(string header, object content, int indentLevel)
+    {
+        if (indentLevel is 0)
+        {
+            yield return header;
+            yield return "{";
+
+            if (content is IEnumerable<string> enumerable)
+                foreach (var s in enumerable)
+                    yield return $"{Indent(1)}{s}";
+            else
+                yield return $"{Indent(1)}{content}";
+
+            yield return "}";
+        }
+        else
+        {
+            var indent = Indent(indentLevel);
+
+            yield return $"{indent}{header}";
+            yield return string.Intern($"{indent}{{");
+
+            if (content is IEnumerable<string> enumerable)
+                foreach (var s in enumerable)
+                    yield return $"{Indent(indentLevel + 1)}{s}";
+            else
+                yield return $"{Indent(indentLevel + 1)}{content}";
+
+            yield return string.Intern($"{indent}}}");
+
+        }
+
+    }
     public static string ToAlphaNumericMethodName(this string txt)
     {
         var arr = new char[txt.Length];
