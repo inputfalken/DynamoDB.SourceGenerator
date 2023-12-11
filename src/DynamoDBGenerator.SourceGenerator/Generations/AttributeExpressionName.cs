cdd91c0d4b68efd5ac1d065c30999f39ac0f2529
inputfalken <inputfalken@gmail.com>
2023-12-11 12:56:21 +0100

    Remove nested functions

--- src/DynamoDBGenerator.SourceGenerator/Generations/AttributeExpressionName.cs
+++ src/DynamoDBGenerator.SourceGenerator/Generations/AttributeExpressionName.cs
@@ -7,7 +7,9 @@ namespace DynamoDBGenerator.SourceGenerator.Generations;
 public static class AttributeExpressionName
 {
 
-    internal static readonly Func<ITypeSymbol, string> TypeName = TypeExtensions.SuffixedTypeSymbolNameFactory("Names", SymbolEqualityComparer.Default);
+    private const string ConstructorAttributeName = "nameRef";
+
+    private static readonly Func<ITypeSymbol, string> TypeName = TypeExtensions.SuffixedTypeSymbolNameFactory("Names", SymbolEqualityComparer.Default);
     internal static IEnumerable<string> CreateClasses(IEnumerable<DynamoDBMarshallerArguments> arguments, Func<ITypeSymbol, IReadOnlyList<DynamoDbDataMember>> getDynamoDbProperties)
     {
         // Using _comparer can double classes when there's a None nullable property mixed with a nullable property
@@ -17,16 +19,55 @@ public static class AttributeExpressionName
             .SelectMany(x => Conversion.ConversionMethods(x.EntityTypeSymbol, y => CreateStruct(y, getDynamoDbProperties), hashSet)).SelectMany(x => x.Code);
 
     }
-
-    internal static (string method, string typeName) RootSignature(ITypeSymbol typeSymbol)
+    private static IEnumerable<string> CreateCode(
+        ITypeSymbol typeSymbol,
+        (bool IsUnknown, TypeIdentifier typeIdentifier, DynamoDbDataMember DDB, string NameRef, string AttributeReference, string AttributeInterfaceName)[] dataMembers,
+        string structName)
     {
-        var typeName = TypeName(typeSymbol);
-        return ($"public {typeName} {AttributeExpressionNameTrackerMethodName}() => new {typeName}(null);", typeName);
-    }
+        const string self = "_self";
+        var constructorFieldAssignments = dataMembers
+            .Select(x =>
+            {
+                var ternaryExpressionName = $"{ConstructorAttributeName} is null ? {@$"""#{x.DDB.AttributeName}"""}: {@$"$""{{{ConstructorAttributeName}}}.#{x.DDB.AttributeName}"""}";
+                return x.IsUnknown
+                    ? $"_{x.DDB.DataMember.Name} = new (() => new {x.AttributeReference}({ternaryExpressionName}));"
+                    : $"{x.NameRef} = new (() => {ternaryExpressionName});";
+            })
+            .Append($@"{self} = new(() => {ConstructorAttributeName} ?? throw new NotImplementedException(""Root element AttributeExpressionName reference.""));");
 
+        foreach (var fieldAssignment in $"public {structName}(string? {ConstructorAttributeName})".CreateBlock(constructorFieldAssignments))
+            yield return fieldAssignment;
+
+        foreach (var fieldDeclaration in dataMembers)
+        {
+            if (fieldDeclaration.IsUnknown)
+            {
+                yield return $"private readonly Lazy<{fieldDeclaration.AttributeReference}> _{fieldDeclaration.DDB.DataMember.Name};";
+                yield return $"public {fieldDeclaration.AttributeReference} {fieldDeclaration.DDB.DataMember.Name} => _{fieldDeclaration.DDB.DataMember.Name}.Value;";
+            }
+            else
+            {
+                yield return $"private readonly Lazy<string> {fieldDeclaration.NameRef};";
+                yield return $"public string {fieldDeclaration.DDB.DataMember.Name} => {fieldDeclaration.NameRef}.Value;";
+
+            }
+        }
+        yield return $"private readonly Lazy<string> {self};";
+
+        var yields = dataMembers
+            .Select(static x => x.IsUnknown
+                ? $"if (_{x.DDB.DataMember.Name}.IsValueCreated) foreach (var x in ({x.DDB.DataMember.Name} as {x.AttributeInterfaceName}).{AttributeExpressionNameTrackerInterfaceAccessedNames}()) {{ yield return x; }}"
+                : $@"if ({x.NameRef}.IsValueCreated) yield return new ({x.NameRef}.Value, ""{x.DDB.AttributeName}"");"
+            )
+            .Append($@"if ({self}.IsValueCreated) yield return new ({self}.Value, ""{typeSymbol.Name}"");");
+
+        foreach (var s in $"IEnumerable<KeyValuePair<string, string>> {AttributeExpressionNameTrackerInterface}.{AttributeExpressionNameTrackerInterfaceAccessedNames}()".CreateBlock(yields))
+            yield return s;
+
+        yield return $"public override string ToString() => {self}.Value;";
+    }
     private static Conversion CreateStruct(ITypeSymbol typeSymbol, Func<ITypeSymbol, IReadOnlyList<DynamoDbDataMember>> fn)
     {
-        const string constructorAttributeName = "nameRef";
         var dataMembers = fn(typeSymbol)
             .Select(x =>
             {
@@ -48,55 +89,14 @@ public static class AttributeExpressionName
 
         var structName = TypeName(typeSymbol);
 
-        var @class = $"public readonly struct {structName} : {AttributeExpressionNameTrackerInterface}"
-            .CreateBlock(CreateCode());
+        var @class = $"public readonly struct {structName} : {AttributeExpressionNameTrackerInterface}".CreateBlock(CreateCode(typeSymbol, dataMembers, structName));
         return new Conversion(@class, dataMembers.Select(x => x.typeIdentifier).OfType<UnknownType>().Select(x => x.TypeSymbol));
-
-        IEnumerable<string> CreateCode()
-        {
-            const string self = "_self";
-            var constructorFieldAssignments = dataMembers
-                .Select(x =>
-                {
-                    var ternaryExpressionName = $"{constructorAttributeName} is null ? {@$"""#{x.DDB.AttributeName}"""}: {@$"$""{{{constructorAttributeName}}}.#{x.DDB.AttributeName}"""}";
-                    return x.IsUnknown
-                        ? $"_{x.DDB.DataMember.Name} = new (() => new {x.AttributeReference}({ternaryExpressionName}));"
-                        : $"{x.NameRef} = new (() => {ternaryExpressionName});";
-                })
-                .Append($@"{self} = new(() => {constructorAttributeName} ?? throw new NotImplementedException(""Root element AttributeExpressionName reference.""));");
-
-            foreach (var fieldAssignment in $"public {structName}(string? {constructorAttributeName})".CreateBlock(constructorFieldAssignments))
-                yield return fieldAssignment;
-
-            foreach (var fieldDeclaration in dataMembers)
-            {
-                if (fieldDeclaration.IsUnknown)
-                {
-                    yield return $"private readonly Lazy<{fieldDeclaration.AttributeReference}> _{fieldDeclaration.DDB.DataMember.Name};";
-                    yield return $"public {fieldDeclaration.AttributeReference} {fieldDeclaration.DDB.DataMember.Name} => _{fieldDeclaration.DDB.DataMember.Name}.Value;";
-                }
-                else
-                {
-                    yield return $"private readonly Lazy<string> {fieldDeclaration.NameRef};";
-                    yield return $"public string {fieldDeclaration.DDB.DataMember.Name} => {fieldDeclaration.NameRef}.Value;";
-
-                }
-            }
-            yield return $"private readonly Lazy<string> {self};";
-
-            var yields = dataMembers
-                .Select(static x => x.IsUnknown
-                    ? $"if (_{x.DDB.DataMember.Name}.IsValueCreated) foreach (var x in ({x.DDB.DataMember.Name} as {x.AttributeInterfaceName}).{AttributeExpressionNameTrackerInterfaceAccessedNames}()) {{ yield return x; }}"
-                    : $@"if ({x.NameRef}.IsValueCreated) yield return new ({x.NameRef}.Value, ""{x.DDB.AttributeName}"");"
-                )
-                .Append($@"if ({self}.IsValueCreated) yield return new ({self}.Value, ""{typeSymbol.Name}"");");
-
-            foreach (var s in $"IEnumerable<KeyValuePair<string, string>> {AttributeExpressionNameTrackerInterface}.{AttributeExpressionNameTrackerInterfaceAccessedNames}()".CreateBlock(yields))
-                yield return s;
-
-            yield return $"public override string ToString() => {self}.Value;";
-        }
 
     }
 
+    internal static (string method, string typeName) RootSignature(ITypeSymbol typeSymbol)
+    {
+        var typeName = TypeName(typeSymbol);
+        return ($"public {typeName} {AttributeExpressionNameTrackerMethodName}() => new {typeName}(null);", typeName);
+    }
 }
--- src/DynamoDBGenerator.SourceGenerator/Generations/AttributeExpressionValue.cs
+++ src/DynamoDBGenerator.SourceGenerator/Generations/AttributeExpressionValue.cs
@@ -5,58 +5,13 @@ namespace DynamoDBGenerator.SourceGenerator.Generations;
 
 public static class AttributeExpressionValue
 {
-    private static readonly Func<ITypeSymbol, string> TypeName = TypeExtensions.SuffixedTypeSymbolNameFactory("Values", SymbolEqualityComparer.Default);
 
     private static readonly Func<ITypeSymbol, string> GetAttributeValueInterfaceName = TypeExtensions.CacheFactory(SymbolEqualityComparer.IncludeNullability,
         x => $"{Constants.DynamoDBGenerator.Marshaller.AttributeExpressionValueTrackerInterface}<{DynamoDbMarshaller.TypeName(x).annotated}>");
 
-    internal static (IEnumerable<string> method, string typeName) RootSignature(ITypeSymbol typeSymbol)
-    {
-        var typeName = TypeName(typeSymbol);
-        return ($"public {typeName} {Constants.DynamoDBGenerator.Marshaller.AttributeExpressionValueTrackerMethodName}()".CreateBlock(
-            "var incrementer = new DynamoExpressionValueIncrementer();",
-            $"return new {typeName}(incrementer.GetNext);"
-        ), typeName);
-    }
-    internal static IEnumerable<string> CreateExpressionAttributeValue(IEnumerable<DynamoDBMarshallerArguments> arguments, Func<ITypeSymbol, IReadOnlyList<DynamoDbDataMember>> getDynamoDbProperties)
-    {
-        // Using _comparer can double classes when there's a None nullable property mixed with a nullable property
-        var hashSet = new HashSet<ITypeSymbol>(SymbolEqualityComparer.Default);
-
-        return arguments
-            .SelectMany(x => Conversion.ConversionMethods(x.ArgumentType, y => CreateStruct(y, getDynamoDbProperties), hashSet)).SelectMany(x => x.Code);
-    }
+    private static readonly Func<ITypeSymbol, string> TypeName = TypeExtensions.SuffixedTypeSymbolNameFactory("Values", SymbolEqualityComparer.Default);
 
     private const string ValueProvider = "valueIdProvider";
-    private static Conversion CreateStruct(ITypeSymbol typeSymbol, Func<ITypeSymbol, IReadOnlyList<DynamoDbDataMember>> fn)
-    {
-        var dataMembers = fn(typeSymbol)
-            .Select(x =>
-            {
-                var typeIdentifier = DynamoDbMarshaller.TypeIdentifier(x.DataMember.Type);
-                var valueRef = $"_{x.DataMember.Name}ValueRef";
-                var attributeReference = TypeName(x.DataMember.Type);
-                var isUnknown = typeIdentifier is UnknownType;
-
-                return (
-                    IsUnknown: isUnknown,
-                    DDB: x,
-                    ValueRef: valueRef,
-                    AttributeReference: attributeReference,
-                    AttributeInterfaceName: GetAttributeValueInterfaceName(x.DataMember.Type),
-                    typeIdentifier
-                );
-            })
-            .ToArray();
-
-        var structName = TypeName(typeSymbol);
-        var interfaceName = GetAttributeValueInterfaceName(typeSymbol);
-
-        var @struct = $"public readonly struct {structName} : {interfaceName}".CreateBlock(CreateCode(typeSymbol, dataMembers, structName, interfaceName));
-
-        return new Conversion(@struct, dataMembers.Select(x => x.typeIdentifier).OfType<UnknownType>().Select(x => x.TypeSymbol));
-
-    }
     private static IEnumerable<string> CreateCode(
         ITypeSymbol typeSymbol,
         (bool IsUnknown, DynamoDbDataMember DDB, string ValueRef, string AttributeReference, string AttributeInterfaceName, TypeIdentifier typeIdentifier)[] dataMembers,
@@ -119,5 +74,51 @@ public static class AttributeExpressionValue
             yield return yield;
 
         yield return $"public override string ToString() => {self}.Value;";
+    }
+    internal static IEnumerable<string> CreateExpressionAttributeValue(IEnumerable<DynamoDBMarshallerArguments> arguments, Func<ITypeSymbol, IReadOnlyList<DynamoDbDataMember>> getDynamoDbProperties)
+    {
+        // Using _comparer can double classes when there's a None nullable property mixed with a nullable property
+        var hashSet = new HashSet<ITypeSymbol>(SymbolEqualityComparer.Default);
+
+        return arguments
+            .SelectMany(x => Conversion.ConversionMethods(x.ArgumentType, y => CreateStruct(y, getDynamoDbProperties), hashSet)).SelectMany(x => x.Code);
+    }
+    private static Conversion CreateStruct(ITypeSymbol typeSymbol, Func<ITypeSymbol, IReadOnlyList<DynamoDbDataMember>> fn)
+    {
+        var dataMembers = fn(typeSymbol)
+            .Select(x =>
+            {
+                var typeIdentifier = DynamoDbMarshaller.TypeIdentifier(x.DataMember.Type);
+                var valueRef = $"_{x.DataMember.Name}ValueRef";
+                var attributeReference = TypeName(x.DataMember.Type);
+                var isUnknown = typeIdentifier is UnknownType;
+
+                return (
+                    IsUnknown: isUnknown,
+                    DDB: x,
+                    ValueRef: valueRef,
+                    AttributeReference: attributeReference,
+                    AttributeInterfaceName: GetAttributeValueInterfaceName(x.DataMember.Type),
+                    typeIdentifier
+                );
+            })
+            .ToArray();
+
+        var structName = TypeName(typeSymbol);
+        var interfaceName = GetAttributeValueInterfaceName(typeSymbol);
+
+        var @struct = $"public readonly struct {structName} : {interfaceName}".CreateBlock(CreateCode(typeSymbol, dataMembers, structName, interfaceName));
+
+        return new Conversion(@struct, dataMembers.Select(x => x.typeIdentifier).OfType<UnknownType>().Select(x => x.TypeSymbol));
+
+    }
+
+    internal static (IEnumerable<string> method, string typeName) RootSignature(ITypeSymbol typeSymbol)
+    {
+        var typeName = TypeName(typeSymbol);
+        return ($"public {typeName} {Constants.DynamoDBGenerator.Marshaller.AttributeExpressionValueTrackerMethodName}()".CreateBlock(
+            "var incrementer = new DynamoExpressionValueIncrementer();",
+            $"return new {typeName}(incrementer.GetNext);"
+        ), typeName);
     }
 }
