4f7d711159d138f12e3ad5dd0cd82467c841d80b
Robert Andersson <robert.andersson.swe@gmail.com>
2023-12-29 10:57:43 +0100

    Add docs

--- src/DynamoDBGenerator/Converters/AttributeValueConverters.cs
+++ src/DynamoDBGenerator/Converters/AttributeValueConverters.cs
@@ -4,13 +4,44 @@ using DynamoDBGenerator.Internal.Converters;
 
 namespace DynamoDBGenerator.Converters;
 
+/// <summary>
+/// Contains the default AttributeValue Converters.
+/// You can inherit from this class in order to extend the default behaviour.
+/// </summary>
 public class AttributeValueConverters
 {
+    /// <summary>
+    /// The <see cref="string"/> converter.
+    /// </summary>
     public IReferenceTypeConverter<string> StringConverter { get; set; } = new StringConverter();
+
+    /// <summary>
+    /// The <see cref="DateTime"/> converter.
+    /// </summary>
     public IValueTypeConverter<DateTime> DateTimeConverter { get; set; } = new ISO8601DateTimeConverter();
+
+    /// <summary>
+    /// The <see cref="DateTimeOffsetConverter"/> converter.
+    /// </summary>
     public IValueTypeConverter<DateTimeOffset> DateTimeOffsetConverter { get; set; } = new ISO8601DateTimeOffsetConverter();
+
+    /// <summary>
+    /// The <see cref="bool"/> converter.
+    /// </summary>
     public IValueTypeConverter<bool> BoolConverter { get; set; } = new BoolConverter();
+
+    /// <summary>
+    /// The <see cref="char"/> converter.
+    /// </summary>
     public IValueTypeConverter<char> CharConverter { get; set; } = new CharConverter();
+
+    /// <summary>
+    /// The <see cref="MemoryStream"/> converter.
+    /// </summary>
     public IReferenceTypeConverter<MemoryStream> MemoryStreamConverter { get; set; } = new MemoryStreamConverter();
+
+    /// <summary>
+    /// The <see cref="int"/> converter.
+    /// </summary>
     public IValueTypeConverter<int> Int32Converter { get; set; } = new Int32Converter();
 }
--- src/DynamoDBGenerator/Converters/IAttributeValueConverter.cs
+++ src/DynamoDBGenerator/Converters/IAttributeValueConverter.cs
@@ -2,14 +2,70 @@ using Amazon.DynamoDBv2.Model;
 
 namespace DynamoDBGenerator.Converters;
 
+/// <summary>
+/// Represents a converter for <see cref="AttributeValue"/> where <typeparamref name="T"/> must be a reference type.
+/// </summary>
+/// <typeparam name="T">
+/// The type to convert To and From.
+/// </typeparam>
 public interface IReferenceTypeConverter<T> where T : class
 {
+    /// <summary>
+    /// A function responsible for converting <see cref="AttributeValue"/> towards <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="attributeValue">
+    /// The value map into <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// The <typeparamref name="T"/> or <see langword="null" />.
+    /// </returns>
+    /// <remarks>
+    /// You should return <see langword="null" /> if you're unable to perform the mapping.
+    /// </remarks>
     public T? Read(AttributeValue attributeValue);
+    
+    /// <summary>
+    /// A function responsible for converting <typeparamref name="T"/> towards <see cref="AttributeValue"/>
+    /// </summary>
+    /// <param name="element">
+    /// The value to write into an <see cref="AttributeValue"/>.
+    /// </param>
+    /// <returns>
+    /// A mapped <see cref="AttributeValue"/>.
+    /// </returns>
     public AttributeValue Write(T element);
 }
 
+/// <summary>
+/// Represents a converter for <see cref="AttributeValue"/> where <typeparamref name="T"/> must be a value type.
+/// </summary>
+/// <typeparam name="T">
+/// The type to convert To and From.
+/// </typeparam>
 public interface IValueTypeConverter<T> where T : struct
 {
+    /// <summary>
+    /// A function responsible for converting <see cref="AttributeValue"/> towards <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="attributeValue">
+    /// The value map into <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    /// The <typeparamref name="T"/> or <see langword="null" />.
+    /// </returns>
+    /// <remarks>
+    /// You should return <see langword="null" /> if you're unable to perform the mapping.
+    /// </remarks>
     public T? Read(AttributeValue attributeValue);
+
+    /// <summary>
+    /// A function responsible for converting <typeparamref name="T"/> towards <see cref="AttributeValue"/>
+    /// </summary>
+    /// <param name="element">
+    /// The value to write into an <see cref="AttributeValue"/>.
+    /// </param>
+    /// <returns>
+    /// A mapped <see cref="AttributeValue"/>.
+    /// </returns>
     public AttributeValue Write(T element);
 }
