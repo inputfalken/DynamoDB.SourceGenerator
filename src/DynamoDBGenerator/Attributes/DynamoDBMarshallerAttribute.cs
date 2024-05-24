bcbfe30a833472b87f866a79738c2aaba4e35e14
Robert Andersson <inputfalken@gmail.com>
2024-05-24 17:11:17 +0200

    Update docs

--- src/DynamoDBGenerator/Attributes/DynamoDBMarshallerAttribute.cs
+++ src/DynamoDBGenerator/Attributes/DynamoDBMarshallerAttribute.cs
@@ -30,17 +30,26 @@ public class DynamoDBMarshallerAttribute : Attribute
     /// <summary>
     /// Gets or sets the type that will be used for marshalling and unmarshalling.
     /// </summary>
+    /// <remarks>
+    /// The default value will be the type where the attribute was applied to.
+    /// </remarks>
     public Type? EntityType { get; set; }
 
     /// <summary>
     /// Gets or sets the name of the property to use when accessing the marshaller.
     /// </summary>
+    /// <remarks>
+    /// The default value will be dependant on the <see cref="EntityType"/> by having the naming format of `{Type.Name}Marshaller`.
+    /// </remarks>
     public string? PropertyName { get; set; }
 
     /// <summary>
     /// Gets or sets the type that <see cref="IDynamoDBMarshaller{TEntity,TArg,TEntityAttributeNameTracker,TArgumentAttributeValueTracker}"/>
     /// will use as its argument type-parameter.
     /// </summary>
+    /// <remarks>
+    /// The default value will be <see cref="EntityType"/>, this is will make the the generated code be implemented in a PUT oriented manner.
+    /// </remarks>
     public Type? ArgumentType { get; set; }
 
 }
