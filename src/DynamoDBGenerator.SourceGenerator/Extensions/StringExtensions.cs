a057e7575264f0f60ec37639f9cab6fe86d90a12
Robert Andersson <inputfalken@gmail.com>
2025-05-24 19:07:41 +0200

    Use span over array

--- src/DynamoDBGenerator.SourceGenerator/Extensions/StringExtensions.cs
+++ src/DynamoDBGenerator.SourceGenerator/Extensions/StringExtensions.cs
@@ -6,21 +6,31 @@ public static class StringExtensions
 {
     public static string ToCamelCaseFromPascal(this string str, [CallerMemberName] string? memberName = null)
     {
-        return ToCamelCaseFromPascal(str.AsSpan(), memberName).ToString();
+        var span = str.AsSpan();
+        if (span.Length is 0)
+            throw new ArgumentException($"Null or Empty string was provided from '{memberName}'");
+
+        if (char.IsLower(span[0]))
+            return str;
+
+        Span<char> array = stackalloc char[span.Length];
+
+        array[0] = char.ToLowerInvariant(span[0]);
+
+        // Skip first element since we handled it manually.
+        for (var i = 1; i < span.Length; i++)
+            array[i] = span[i];
+
+        return array.ToString();
     }
 
     public static string ToPrivateFieldFromPascal(this string str, [CallerMemberName] string? memberName = null)
     {
-        return ToPrivateFieldFromPascal(str.AsSpan(), memberName).ToString();
-    }
-
-    public static ReadOnlySpan<char> ToPrivateFieldFromPascal(this ReadOnlySpan<char> span,
-        [CallerMemberName] string? memberName = null)
-    {
+        var span = str.AsSpan();
         if (span.Length is 0)
             throw new ArgumentException($"Null or Empty string was provided from '{memberName}'");
 
-        var array = new char[span.Length + 1];
+        Span<char> array = stackalloc char[span.Length + 1];
 
         array[0] = '_';
         array[1] = char.ToLowerInvariant(span[0]);
@@ -29,30 +39,11 @@ public static class StringExtensions
         for (var i = 1; i < span.Length; i++)
             array[i + 1] = span[i];
 
-        return array;
+        return array.ToString();
     }
 
-    public static ReadOnlySpan<char> ToCamelCaseFromPascal(this ReadOnlySpan<char> span,
-        [CallerMemberName] string? memberName = null)
-    {
-        if (span.Length is 0)
-            throw new ArgumentException($"Null or Empty string was provided from '{memberName}'");
-
-        if (char.IsLower(span[0]))
-            return span;
-
-        var array = new char[span.Length];
-
-        array[0] = char.ToLowerInvariant(span[0]);
-
-        // Skip first element since we handled it manually.
-        for (var i = 1; i < span.Length; i++)
-            array[i] = span[i];
-
-        return array;
-    }
-
-    public static IEnumerable<string> ScopeTo(this IEnumerable<string> content, string header) => CreateScope(header, content);
+    public static IEnumerable<string> ScopeTo(this IEnumerable<string> content, string header) =>
+        CreateScope(header, content);
 
     public static IEnumerable<string> CreateScope(this string header, IEnumerable<string> content) => content
         .Select(x => $"    {x}")
@@ -83,15 +74,13 @@ public static class StringExtensions
 
     public static string ToAlphaNumericMethodName(this string txt)
     {
-        var arr = new char[txt.Length];
         var index = 0;
 
-        for (var i = 0; i < txt.Length; i++)
-        {
-            var c = txt[i];
-            if (char.IsLetter(c) || (index > 0 && char.IsNumber(c))) arr[index++] = c;
-        }
+        Span<char> arr = stackalloc char[txt.Length];
+        foreach (var c in txt.AsSpan())
+            if (char.IsLetter(c) || (index > 0 && char.IsNumber(c)))
+                arr[index++] = c;
 
-        return new string(arr, 0, index);
+        return arr.Slice(0, index).ToString();
     }
 }
