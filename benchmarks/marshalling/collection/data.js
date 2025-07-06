d8183649c8a6abe8c07cdef1a856cb92f74d7ca6
github-action-benchmark <github@users.noreply.github.com>
2025-07-06 08:53:02 +0000

    add Collection (benchmarkdotnet) benchmark result for e72aa8facb6e64fedffabbf5e6ff97f80ca8d2b7

---
+++ benchmarks/marshalling/collection/data.js
@@ -0,0 +1,92 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1751791982050,
+  "repoUrl": "https://github.com/inputfalken/DynamoDB.SourceGenerator",
+  "entries": {
+    "Collection": [
+      {
+        "commit": {
+          "author": {
+            "email": "inputfalken@gmail.com",
+            "name": "Robert Andersson",
+            "username": "inputfalken"
+          },
+          "committer": {
+            "email": "inputfalken@gmail.com",
+            "name": "Robert Andersson",
+            "username": "inputfalken"
+          },
+          "distinct": true,
+          "id": "e72aa8facb6e64fedffabbf5e6ff97f80ca8d2b7",
+          "message": "Split benchmark into different executions",
+          "timestamp": "2025-07-06T10:49:06+02:00",
+          "tree_id": "662196d99d5adda1ce660a979abadbce93f767b2",
+          "url": "https://github.com/inputfalken/DynamoDB.SourceGenerator/commit/e72aa8facb6e64fedffabbf5e6ff97f80ca8d2b7"
+        },
+        "date": 1751791981706,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Unmarshall_Dictionary",
+            "value": 243.43626436820398,
+            "unit": "ns",
+            "range": "± 2.3580051948486207"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Marshall_Dictionary",
+            "value": 312.7486159618084,
+            "unit": "ns",
+            "range": "± 2.3478097219516587"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Unmarshall_StringHashSet",
+            "value": 100.54272908889331,
+            "unit": "ns",
+            "range": "± 1.0406338286519954"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Marshall_StringHashSet",
+            "value": 142.28655212720236,
+            "unit": "ns",
+            "range": "± 1.3229120493042505"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Unmarshall_StringList",
+            "value": 232.3557046822139,
+            "unit": "ns",
+            "range": "± 0.8582023464138998"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Marshall_StringList",
+            "value": 336.69256767859827,
+            "unit": "ns",
+            "range": "± 2.3821029856395906"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Unmarshall_IntHashSet",
+            "value": 92.10358840227127,
+            "unit": "ns",
+            "range": "± 0.6546051033110679"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Marshall_IntHashSet",
+            "value": 110.60281756094524,
+            "unit": "ns",
+            "range": "± 1.5563471183695134"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Unmarshall_KeyValuePairList",
+            "value": 383.7711319605509,
+            "unit": "ns",
+            "range": "± 5.191151040075087"
+          },
+          {
+            "name": "DynamoDBGenerator.SourceGenerator.Benchmarks.Benchmarks.Marshalling.CollectionBenchmarks.Marshall_KeyValuePairList",
+            "value": 699.8691508219792,
+            "unit": "ns",
+            "range": "± 8.303172969607951"
+          }
+        ]
+      }
+    ]
+  }
+}
